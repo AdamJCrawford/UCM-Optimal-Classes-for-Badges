70cdbcf465b1e84fe09360b683191d817324c77c
Adam Crawford <adamcrawford253@gmail.com>
2022-11-11 23:01:11 -0800

    Small file cleanup

--- Get_Badge_Information.py
+++ Get_Badge_Information.py
@@ -7,22 +7,22 @@ def fetch(badges: list[str]):
     all_options = []
 
     if "Div" in badges:
-        with open('DiversityandIdentity.txt') as f:
+        with open("Classes/DiversityandIdentity.txt") as f:
             all_options.append([f.readline()[:-1] for line in f])
     if "Med" in badges:
-        with open('MediaandVisualAnalysis.txt') as f:
+        with open("Classes/MediaandVisualAnalysis.txt") as f:
             all_options.append([f.readline()[:-1] for line in f])
     if "Soc" in badges:
-        with open('SocietiesandCulturesofthePast.txt') as f:
+        with open("Classes/SocietiesandCulturesofthePast.txt") as f:
             all_options.append([f.readline()[:-1] for line in f])
     if "Glo" in badges:
-        with open('GlobalAwareness.txt') as f:
+        with open("Classes/GlobalAwareness.txt") as f:
             all_options.append([f.readline()[:-1] for line in f])
     if "Eth" in badges:
-        with open('Ethics.txt') as f:
+        with open("Classes/Ethics.txt") as f:
             all_options.append([f.readline()[:-1] for line in f])
     if "Sus" in badges:
-        with open('Sustainability.txt') as f:
+        with open("Classes/Sustainability.txt") as f:
             all_options.append([f.readline()[:-1] for line in f])
 
     return Counter(chain(*all_options))
--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 import GUI
 import Get_Badge_Information as GBI
+
+
+print(GBI.fetch(["Soc", "Med"]))
